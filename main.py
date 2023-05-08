cae9efd87cc9b44d0f4f1b782c48b185be8a2ade
David Ghiberdic Cristian <68118099+MUSHcoat@users.noreply.github.com>
2023-05-08 22:51:19 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import time
 import os
+import datetime as dt
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
+from lora import transmitPackets
 
 from bme280 import BME
 from mq9 import MQ
 from gp2y import GP2Y
 
 
-
 #general defintions
 i2c = busio.I2C(board.SCL, board.SDA)
-
-pin_led = 12
-pin_vo = 40
-pin_vled = 38
         
 # define sensors
 BME = BME(decimal=5)
@@ -48,4 +45,12 @@ if __name__ == '__main__':
                 print('')
 
         iteration += 1
+        print(f"{now.strftime('%d/%m, %H:%M:%S')};{get_current_time()};{BMP.press(4)},{BMP.alt(4)};{HDC.temp(4)},{HDC.hum(4)};{NEO_la},{NEO_lo};{MPU.accel()};{MPU.gyros()};{MPU.magnet()}")        
+        
+        now = dt.datetime.now()
+        payload = f"{now.strftime('%d/%m, %H:%M:%S')};{get_current_time()};{BMP.press(4)},{BMP.alt(4)};{HDC.temp(4)},{HDC.hum(4)};{NEO_la},{NEO_lo};{MPU.accel()};{MPU.gyros()};{MPU.magnet()}"
+
+        transmitPackets(payload)
+        print("[ OK ] Payload sent ...")
+        
         time.sleep(1)
